dbdbed2626c03f34da52c1405cd4eb672617357e
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-12-12 19:07:07 +0530

    Add alpstein-tui formula

---
+++ Formula/alpstein-tui.rb
@@ -0,0 +1,11 @@
+class AlpsteinTui < Formula
+  desc "Alpstein CLI tool"
+  homepage "https://github.com/whiplashvin/alpstein-tui"
+  url "https://github.com/whiplashvin/alpstein-tui/releases/download/v0.3/alpstein-tui_0.3_darwin_amd64.tar.gz"
+  sha256 "your-sha256-hash"
+
+  def install
+    bin.install "alpstein-tui"
+  end
+end
+
