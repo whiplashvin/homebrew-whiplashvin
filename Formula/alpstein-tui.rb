a359a73c5fd7c16082934649fb7981e98f4254e3
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-12-12 19:28:19 +0530

    upated-shasum

--- Formula/alpstein-tui.rb
+++ Formula/alpstein-tui.rb
@@ -2,7 +2,7 @@ class AlpsteinTui < Formula
   desc "Alpstein CLI tool"
   homepage "https://github.com/whiplashvin/alpstein-tui"
   url "https://github.com/whiplashvin/alpstein-tui/releases/download/v0.3/alpstein-tui_0.3_darwin_amd64.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "64f4e4d50dae7492a3bf7f3167b2be8f9adad1e22a461a0ce817574212b39707"
 
   def install
     bin.install "alpstein-tui"
