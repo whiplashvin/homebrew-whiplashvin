b30101d1e6d17f1b08575131a89f9703471e451a
arvindkhoisnam <khoisnamarvind@gmail.com>
2026-03-14 16:55:25 +0530

    new version

--- Formula/alpstein-tui.rb
+++ Formula/alpstein-tui.rb
@@ -1,9 +1,9 @@
 class AlpsteinTui < Formula
   desc "Alpstein CLI tool"
   homepage "https://github.com/whiplashvin/alpstein-tui"
-  url "https://github.com/whiplashvin/alpstein-tui/releases/download/v1.0.2/alpstein-tui_1.0.2_darwin_amd64.tar.gz"
-  sha256 "ed9c268b76b78166ffe267fd3aafe1cee154d0b0e50ae279134b9f9f6a5ce38d"
-  version "1.0.2"
+  url "https://github.com/whiplashvin/alpstein-tui/releases/download/v1.0.3/alpstein-tui_1.0.3_darwin_amd64.tar.gz"
+  sha256 "62231a5a8eb882a57b7f1f4a32f106cf99133dae48946796587ea4906fce76b7"
+  version "1.0.3"
 
   def install
     bin.install "alpstein-tui"
